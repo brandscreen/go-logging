5419392465d1cde2e6a2133eed42b145c11de69b
Miles Delahunty <miles.delahunty@gmail.com>
2015-02-18 12:23:19 +1100

    s/pathname/filename/ in default log format

--- logging/brandscreen_logging.go
+++ logging/brandscreen_logging.go
@@ -4,7 +4,7 @@ func BrandscreenLogger(name string, filename string) (*Logger, error) {
 	return FileLogger(
 		name,
 		DEBUG,
-		"%s [%s] %s[%d] %s:%d:%s: %s\ntime,levelname,name,process,pathname,lineno,funcname,message",
+		"%s [%s] %s[%d] %s:%d:%s: %s\ntime,levelname,name,process,filename,lineno,funcname,message",
 		"2006-01-02 15:04:05.000000",
 		filename,
 		false)
